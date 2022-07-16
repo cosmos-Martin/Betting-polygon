512b41e3fab1f2d4de7f44dbab749b37cb8ec9a8
cosmos-Martin <martinelawrence920@gmail.com>
2022-07-16 06:28:25 -0700

    update

--- contracts/betting.sol
+++ contracts/betting.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 contract BET is ERC20{
 
 struct Betting {
@@ -11,42 +10,44 @@ struct Betting {
      bool isCreated;
      bool isStarted;
 }
-
 event bettingCreate(
     address  creator,
     uint256  nftid,
     uint256  price,
     bool    isCreated
 );
-
 event bettingStart(
     address  creator,
     uint256  nftid,
     uint256  price,
-    bool    isStarted
+    bool  isStarted
 );
 
+mapping(uint256=>uint256) prevOracle;
 mapping(uint256=>Betting) BettingList;
 mapping(uint256 => mapping(address => uint256)) collectionAmount;
 
-    constructor(string memory name , string memory symbol) ERC20(name,symbol)  {}
-    function doBetting(uint256 _nftId,uint256 _oraclePrice) public  {
+ constructor(string memory name , string memory symbol) ERC20(name,symbol)  {}
+ 
+    // params _oracleprice price of 1eth
+    function doBetting(uint256 _nftId,uint256 _oraclePrice) public payable {
         require(_oraclePrice!=0,"oracle pricce must not be 0");
-        require(BettingList[_nftId].isStarted==true,"can't do Betting");
+        require(BettingList[_nftId].isStarted == true,"can't do Betting");
         address _ply1 = BettingList[_nftId].player1;
         address _ply2 = BettingList[_nftId].player2;
         uint256 collect1 = collectionAmount[_nftId][_ply1];
         uint256 collect2 = collectionAmount[_nftId][_ply2];
-        uint256 oraclePrice = BettingList[_nftId].curBetamount/_oraclePrice;    
-        if(oraclePrice/collect1*100>60){
-           uint256 x=(5*oraclePrice/2*collect1)/3;
+        uint256 curBettingEth = BettingList[_nftId].curBetamount*prevOracle[_nftId]/_oraclePrice;    
+        if(curBettingEth/collect1*100>60){
+           uint256 x=(5*curBettingEth/2*collect1)/3;
            collacteral( _nftId ,  _ply1 , x);
         }
 
-        if(oraclePrice/collect2*100>60){
-            uint256 x=(5*oraclePrice/2*collect2)/3;
+        if(curBettingEth/collect2*100>60){
+            uint256 x=(5*curBettingEth/2*collect2)/3;
             collacteral( _nftId ,  _ply2 , x);
         }
+        prevOracle[_nftId] = _oraclePrice;
     }
 
     function collacteral( uint256 _nftId , address _target , uint256 _amount) internal {
@@ -56,23 +57,22 @@ mapping(uint256 => mapping(address => uint256)) collectionAmount;
         BettingList[_nftId].curBetamount -=_amount;
     }
 
-    function startBetting(uint256 _nftid, uint256 _BettingPrice) public payable {
+    function startBetting(uint256 _nftid, uint256 _BettingPrice,uint256 oracleprice) public payable {
         require(BettingList[_nftid].isStarted==false,"cant start this Betting");
         require(msg.value>=_BettingPrice,"cant start Betting");
-        if(BettingList[_nftid].isCreated){
+        if(BettingList[_nftid].isCreated == false){
             BettingList[_nftid].player1 = msg.sender;
-            collectionAmount[_nftid][msg.sender]=msg.value;
-            BettingList[_nftid].isStarted=true;
-            emit bettingCreate(BettingList[_nftid].player1,_nftid,collectionAmount[_nftid][msg.sender],BettingList[_nftid].isCreated);
-
-        }else{
             BettingList[_nftid].curBetamount = _BettingPrice;
-            BettingList[_nftid].isCreated=true;
-            BettingList[_nftid].player1 = msg.sender;
+            prevOracle[_nftid] = oracleprice;
+            collectionAmount[_nftid][msg.sender]=msg.value;
+            BettingList[_nftid].isCreated = true;
+            emit bettingCreate(BettingList[_nftid].player1,_nftid,collectionAmount[_nftid][msg.sender],BettingList[_nftid].isCreated);
+        }else{
+            BettingList[_nftid].isStarted=true;
+            BettingList[_nftid].player2 = msg.sender;
             collectionAmount[_nftid][msg.sender]=msg.value;
             emit bettingStart(BettingList[_nftid].player2,_nftid,collectionAmount[_nftid][msg.sender],BettingList[_nftid].isStarted);
-
         }
     }
-
 }
+
--- test/betting.js
+++ test/betting.js
@@ -1,39 +1,74 @@
+
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers , waffle } = require("hardhat");
 
 describe("betting contract", async () => {
   let bettingContract;
   let owner,player1,player2;
+
   beforeEach(async () => {
     const bettingToken = await ethers.getContractFactory("BET");
-    [owner,player1,player2] = await ethers.getSigners();  
+    [owner,player1,player2] = await ethers.getSigners();
      bettingContract = await bettingToken.deploy("bettingToken","BT");
      bettingContract = await bettingContract.deployed();
     console.log("contract address-----------",bettingContract.address);
     console.log("deployer address----------",owner.address);
     console.log("player1 address----------",player1.address);
     console.log("player2 address----------",player2.address);
-  });
+    let provider = ethers.getDefaultProvider();
+    const balance1 = await provider.getBalance(player1.address);
+    const balance2 = await provider.getBalance(player2.address);
+    // console.log(balance.toString()); // prints 100000000000... 18 zeros
 
-  it("create vetting",async function(){
+    // to format the value in a readable format
+    console.log("player1 balance first",ethers.utils.formatEther(balance1)); // prints 1.0
+    console.log("player2 balance first",ethers.utils.formatEther(balance2)); // prints 1.0
     let bettingPrice = ethers.utils.parseEther("90");
     let player1price = ethers.utils.parseEther("130");
     let player2price = ethers.utils.parseEther("300");
 
-    await expect(bettingContract.connect(player1).startBetting(1,bettingPrice,{value:player1price}))
-    .to.emit(bettingContract, "bettingStart")
-    .withArgs(player1.address,1,player1price, true);
-    // await expect(bettingContract.connect(player2).startBetting(1,bettingPrice,{value:player2price}));
-  })
+    await bettingContract.connect(player1).startBetting(1,bettingPrice,1000,{value:player1price});
+    await bettingContract.connect(player2).startBetting(1,bettingPrice,1000,{value:player2price});
+    
+  });
+ 
+  // it("create vetting",async function(){
 
-  // it("do vetting",async function(){
-  //   let bettingPrice = ethers.utils.parseEther("90");
-  //   let player1price = ethers.utils.parseEther("130");
-  //   let player2price = ethers.utils.parseEther("300");
-  //   await expect(bettingContract.connect(player1).startBetting(1,bettingPrice,{value:player1price}))
-  //   .to.emit(bettingContract, "bettingStart").withArgs(player1.address,1,player1price, true);
-  //   await expect(bettingContract.connect(player2).startBetting(1,bettingPrice,{value:player2price}));
+    
+  //   // await expect(bettingContract.connect(player1).startBetting(1,bettingPrice,{value:player1price})).to.emit(bettingContract, "bettingStart").withArgs(player1.address,1,player1price, true);
+  //   // await expect(bettingContract.connect(player1).startBetting(1,bettingPrice,{value:player1price}));
+  //    //await expect(bettingContract.connect(player2).startBetting(1,bettingPrice,{value:player2price}));
+    
+    
   // })
+
+  it("do vetting",async function(){
+    let bettingPrice = ethers.utils.parseEther("90");
+    let player1price = ethers.utils.parseEther("330");
+    let player2price = ethers.utils.parseEther("400");
+    // player1balance =await player1.getBalance();
+    let provider = ethers.getDefaultProvider();
+    const balance1 = await provider.getBalance(player1.address);
+    const balance2 = await provider.getBalance(player2.address);
+    // console.log(balance.toString()); // prints 100000000000... 18 zeros
+
+    // to format the value in a readable format
+    console.log("player1 balance before",ethers.utils.formatEther(balance1)); // prints 1.0
+    console.log("player2 balance before",ethers.utils.formatEther(balance2)); // prints 1.0
+    
+    await bettingContract.doBetting(1,400);
+
+     balance1 = await provider.getBalance(player1.address);
+     balance2 = await provider.getBalance(player2.address);
+    // console.log(balance.toString()); // prints 100000000000... 18 zeros
+
+    // to format the value in a readable format
+    console.log("player1 balance after",ethers.utils.formatEther(balance1)); // prints 1.0
+    console.log("player2 balance after",ethers.utils.formatEther(balance2)); // prints 1.0
+    
+    // await bettingContract.connect(player1).startBetting(1,bettingPrice,{value:player1price});
+   // await bettingContract.connect(player2).startBetting(1,bettingPrice,{value:player2price});
+  })
 
 });
 
